e8f385e315454298f6a5aaf84d94023b1fe37c77
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-03-21 14:54:58 +0000

    Create a React app and publish it to GitHub Pages

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+  
+import React from "react"
+import Link from 'gatsby-link'
+
+import Layout from '../components/layout'
+
+const ErrorPage = () => {
+    return (
+        <Layout>
+            
+            <h1>404</h1>
+            
+        </Layout>
+    )
+}
+
+export default ErrorPage
