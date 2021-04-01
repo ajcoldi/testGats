49540cbb0330e148db4b18eae4461f80c5d49568
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-04-01 22:20:24 +0100

    skills page rehaul

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -10,50 +10,49 @@ const skillsPage = () => {
             
             
                 <div className="skillsWrapper">
-
-
-
+                    <h2>Skills and Knowledge</h2>
                     <div className="skillsText"> 
-                        <div className="skill">
+                    
+                        <div id="skill1" className="skill blue">
                             <h3>HTML</h3>
                             <p>html 4.6 - 5 / Validation</p>
                         </div>
-                        <div className="skill">
+                        <div id="skill2" className="skill">
                             <h3>CSS</h3>
                             <p>Grids / Flex / Bootstrap / SASS </p>
                         </div>
-                        <div className="skill">
+                        <div id="skill3" className="skill blue">
                             <h3>JAVASCRIPT/JQUERY</h3>
                             <p>OOP / UI / Form Validation / API's</p>
                         </div>
-                        <div className="skill">
+                        <div id="skill4" className="skill">
                             <h3>LIGHTBOX</h3>
                         </div>
-                        <div className="skill">
+                        <div id="skill5" className="skill blue">
                             <h3>REACT/GATSBY</h3>
                             <p>Components and Modules / Headless CMS / Plugins</p>
                         </div>
-                        <div className="skill">
+                        <div id="skill6" className="skill">
                             <h3>GIT</h3>
                         </div>
                     </div>
-                   
-                    <div className="knowOf">
                         <h3 className="KOTitle">Knowledge Of:</h3>
-                            <div className="skill">
+                    <div className="knowOf">
+                        
+                            <div id="KO1" className="skill blue">
                                 <h3>PHP</h3>
                                 <p>PDO - MVC / validation / Security / xampp</p>
                             </div>
-                            <div className="skill">
+                            <div id="KO2" className="skill ">
                                 <h3>SQL</h3>
                             </div>
-                            <div className="skill">
+                            <div id="KO3" className="skill blue">
                                 <h3>GRAPHQL</h3>
                             </div>
-                            <div className="skill">
+                            <div id="KO4" className="skill">
                                 <h3>XAMPP</h3>
                             </div>
-                            <div className="skill">
+                            <div id="KO5" className="skill blue">
                                 <h3>NPM</h3>
                             </div>
                             
