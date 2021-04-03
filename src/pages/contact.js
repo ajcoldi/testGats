6d688caf3e72cd910d4a126dc7eb788af08e907d
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-04-03 13:20:38 +0100

    contact page update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,12 +7,14 @@ import Layout from '../components/layout'
 const contactPage = () => {
     return (
         <Layout>
+               <h2>Web Development</h2>
+        
             <div className="formWrapper">
                 
             <form name="contact" method="POST" data-netlify="true">
                 <input type="hidden" name="form-name" value="contact"></input>
                 <input type="text" name="name" placeholder="Name" /> 
-                <input type="email" name="email" placeholder="Name" />
+                <input type="email" name="email" placeholder="Email" />
                 <textarea name="message" placeholder="Enter your message"></textarea>
                 <button type="submit">Send</button>
             </form>
