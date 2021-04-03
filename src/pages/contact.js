2d7ed0f735dac933dafac0b14889c8b62398fc62
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-04-03 12:24:46 +0100

    jksajdsan

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,72 +8,31 @@ const contactPage = () => {
     return (
         <Layout>
             <div className="formWrapper">
-            <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{' '}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'name'}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'text'}
-                      name={'name'}
-                      onChange={this.handleChange}
-                      id={'name'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'email'}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'email'}
-                      name={'email'}
-                      onChange={this.handleChange}
-                      id={'email'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'message'}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={'message'}
-                      onChange={this.handleChange}
-                      id={'message'}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
+            
+            <form name="contactForm" data-netlify="true" method="POST" onSubmit={handleSubmit}>
+            <input type="hidden" name="form-name" value="contactForm" />
+                <p>
+                <label>
+                    Your Name: <input type="text" name="name" value={fullName} onChange={(e)=>handleFullName(e.target.value)} />
+                </label>
+                </p>
+                <p>
+                <label>
+                    Your Email: <input type="email" name="email" value={email} onChange={(e)=>handleEmail(e.target.value)} />
+                </label>
+                </p>
+                <p>
+                <label>
+                    Message: <textarea name="message" value={message} onChange={(e)=>handleMessage(e.target.value)} />
+                </label>
+                </p>
+                <p>
+                <button type="submit">Send</button>
+                </p>
+            </form>
+
+
+
             </div>
         </Layout>
     )
