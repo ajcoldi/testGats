a4a033f6a91a5478ecbb4d627eac4ff74275da80
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-04-03 12:10:35 +0100

    contact update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,26 +8,11 @@ const contactPage = () => {
     return (
         <Layout>
             <div className="formWrapper">
-            <form method="POST" action="/thank-you/english.html" name="contact" data-netlify="true"  data-netlify-honeypot="bot-field" enctype="application/x-www-form-urlencoded"  >
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+            <form data-netlify="true" hidden name="contact" netlify-honeypot="bot-field">
+            <input type="text" name="username" />
+            <input type="email" name="email" />
+            <input name="bot-field" type="hidden" />
+            </form>
             </div>
         </Layout>
     )
