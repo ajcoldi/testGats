f38de86a784a3ea63146372b84aac7895a4c3eee
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-04-03 12:36:55 +0100

    final contact test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,25 +10,11 @@ const contactPage = () => {
             <div className="formWrapper">
                 
             <form name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact"></input>
-            <p>
-                <label>Your Name: <input type="text" name="name" /></label>   
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-                </select></label>
-            </p>
-            <p>
-                <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
+                <input type="hidden" name="form-name" value="contact"></input>
+                <input type="text" name="name" placeholder="Name" /> 
+                <input type="email" name="email" placeholder="Name" />
+                <textarea name="message" placeholder="Enter your message"></textarea>
                 <button type="submit">Send</button>
-            </p>
             </form>
            
 
